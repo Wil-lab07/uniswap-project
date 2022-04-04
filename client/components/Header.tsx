359d35b8e828bb554be4a374911f8fa7034e16d7
William <79161142+Wil-lab07@users.noreply.github.com>
2022-04-05 00:07:07 +0700

    feat: create the hamburger navigation

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,4 +1,5 @@
-import {Box, Flex, Text, Button, Img, HStack} from '@chakra-ui/react'
+import {Box, Flex, Text, Button, Img, HStack, IconButton, useDisclosure, Collapse, Stack} from '@chakra-ui/react'
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 import Link from 'next/link'
 import {useConnect, useAccount, chain, useBalance, InjectedConnector} from 'wagmi'
 
@@ -8,18 +9,85 @@ const Header = () => {
   const [ethersBalance] = useBalance({
     addressOrName: accountData.data?.address
   })
+  const { isOpen, onToggle } = useDisclosure();
   return (
-    <Flex py={2} px={4} bgColor="#191B1F" align={'center'} justify={'space-between'}>
-      <Flex flex={{base: 1}} justify={{base: 'center', md: 'start'}} align="center">
-        <Text>SENDS</Text>
+    <>
+      <Flex py={2} px={4} bgColor="#191B1F" align={'center'} justify={'space-between'}>
+        <Flex flex={{base: 1}} justify={{base: 'center', md: 'start'}} align="center">
+          <Text>SENDS</Text>
+        </Flex>
+        <Flex flex={{base: 1}} justifyContent={'center'} align="center" display={{base: 'flex', md: 'none'}}>
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={'ghost'}
+            aria-label={'Toggle Navigation'}
+          />
+        </Flex>
+        <Flex flex={{base: 1}} justify={'center'} display={{base: 'none', md: 'inline-flex'}} align="center">
+          <HStack spacing={3} bgColor="blackAlpha.200" borderRadius={40}>
+            <Link href="https://www.coingecko.com/"><Button colorScheme={'blackAlpha'} textColor="white" borderRadius={40} size={'sm'} px={5} py={5}>Charts</Button></Link>
+          </HStack>
+        </Flex>
+        <Flex flex={{base: 1}} justify={'center'} align='center' display={{base: 'none', md: 'inline-flex'}}>
+          <HStack spacing={3}>
+            {!connectData.data?.connected ? 
+              <Button
+                colorScheme={'blackAlpha'} 
+                textColor="white" 
+                borderRadius={40} 
+                size={'sm'} 
+                px={2} 
+                py={5}
+                onClick={()=>{connect(connectData.data?.connectors[0])}}
+              >
+                <Text p={2} borderRadius={40} bgColor="#172A42" textColor={'#4F90EA'}>{!connectData.data?.connectors[0].ready ? 'Please Install Metamask' : 'Connect Wallet'}</Text>
+              </Button>
+            :
+              <>
+                <Button textColor="white" colorScheme={'blackAlpha'} borderRadius={40} size={'sm'} px={5} py={5} onClick={disconnect}>
+                  Disconnect
+                </Button>
+                <Button 
+                  colorScheme={'blackAlpha'} 
+                  textColor="white" 
+                  borderRadius={40} 
+                  size={'sm'} 
+                  px={2} 
+                  py={5}
+                  disabled
+                  _disabled={{textColor: 'white'}}
+                >
+                  <Text p={2} borderRadius={40}>{`${accountData.data?.address.slice(0, 7)}...${accountData.data?.address.slice(35)}`}</Text>
+                </Button>
+                <Button textColor="white" colorScheme={'blackAlpha'} borderRadius={40} size={'sm'} px={5} py={5} disabled _disabled={{textColor: 'white'}}>
+                  <Flex>{ethersBalance.data?.formatted.slice(0, 7)}<Img ml={2} src={'/eth.png'} w={'20px'}/></Flex>
+                </Button> 
+              </>
+            }
+          </HStack>
+        </Flex>
       </Flex>
-      <Flex flex={{base: 1}} justify={'center'} display={{base: 'none', md: 'inline-flex'}} align="center">
-        <HStack spacing={3} bgColor="blackAlpha.200" borderRadius={40}>
-          <Link href="https://www.coingecko.com/"><Button colorScheme={'blackAlpha'} textColor="white" borderRadius={40} size={'sm'} px={5} py={5}>Charts</Button></Link>
-        </HStack>
-      </Flex>
-      <Flex flex={{base: 1}} justify={'center'} display={{base: 'none', md: 'inline-flex'}} align='center'>
-        <HStack spacing={3}>
+      <Collapse 
+        style={{
+          position: 'fixed', 
+          width: '100%',
+          top: '50px',
+          background: '#191B1F',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 1
+        }} 
+        in={isOpen} 
+        animateOpacity
+      >
+        <Stack>
+          <Button textColor="white" colorScheme={'blackAlpha'} borderRadius={40} size={'sm'} px={5} py={5}>
+            <Link href="https://www.coingecko.com/">Charts</Link>
+          </Button>
           {!connectData.data?.connected ? 
             <Button
               colorScheme={'blackAlpha'} 
@@ -54,9 +122,9 @@ const Header = () => {
               </Button> 
             </>
           }
-        </HStack>
-      </Flex>
-    </Flex>
+        </Stack> 
+      </Collapse>
+    </>
   );
 }
  
